45e6dbc5a83b636a032fd2f1137fda8bdbc6c914
Denis Pronin <de.pronin@gmail.com>
2024-03-18 15:56:09 +0500

    updated UserServiceCachingMethodInterceptor

--- DynamicProxyExample/src/main/java/com/github/dennispronin/exploring/proxy/dynamic/example/cglib/UserServiceCachingMethodInterceptor.java
+++ DynamicProxyExample/src/main/java/com/github/dennispronin/exploring/proxy/dynamic/example/cglib/UserServiceCachingMethodInterceptor.java
@@ -1,13 +1,33 @@
 package com.github.dennispronin.exploring.proxy.dynamic.example.cglib;
 
+import com.github.dennispronin.exploring.proxy.dynamic.example.UserService;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserServiceCachingMethodInterceptor implements MethodInterceptor {
+
+    private final Map<Long, String> cache;
+
+    public UserServiceCachingMethodInterceptor() {
+        this.cache = new HashMap<>();
+    }
+
     @Override
     public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
-        return proxy.invokeSuper(obj, args);
+        if (!(obj instanceof UserService)) {
+            throw new IllegalArgumentException("Proxy of wrong type passed. Expected type: " + UserService.class);
+        }
+        Long userId = (Long) args[0];
+        if (cache.containsKey(userId)) {
+            return cache.get(userId);
+        } else {
+            String user = (String) proxy.invokeSuper(obj, args);
+            cache.put(userId, user);
+            return user;
+        }
     }
 }
