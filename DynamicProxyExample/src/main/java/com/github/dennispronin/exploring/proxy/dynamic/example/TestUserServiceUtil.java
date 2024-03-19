9c3700faa995f8d74d3a72c557047e1e1ddedec7
Denis Pronin <de.pronin@gmail.com>
2024-03-19 14:10:11 +0500

    updated TestUserServiceUtil

--- DynamicProxyExample/src/main/java/com/github/dennispronin/exploring/proxy/dynamic/example/TestUserServiceUtil.java
+++ DynamicProxyExample/src/main/java/com/github/dennispronin/exploring/proxy/dynamic/example/TestUserServiceUtil.java
@@ -2,23 +2,35 @@ package com.github.dennispronin.exploring.proxy.dynamic.example;
 
 public class TestUserServiceUtil {
 
-    public static void test(UserService userService) {
+    public static void test(UserService proxyService) {
         UserService realUserService = new UserServiceImpl(null);
-        System.out.println("Real object and proxy have same classloader:\n");
-        System.out.println(realUserService.getClass().getClassLoader());
-        System.out.println(userService.getClass().getClassLoader());
-        System.out.println("\nBut different class:");
-        System.out.println(realUserService.getClass());
-        System.out.println(userService.getClass());
+
+        String realServiceClassLoader = realUserService.getClass().getClassLoader().toString();
+        String proxyServiceClassLoader = proxyService.getClass().getClassLoader().toString();
+        if (realServiceClassLoader.equals(proxyServiceClassLoader)) {
+            System.out.println("Real service and proxy have same classloader:\n");
+        } else {
+            System.out.println("Real service and proxy have different classloader:\n");
+        }
+        System.out.println(realServiceClassLoader);
+        System.out.println(proxyServiceClassLoader);
+
+        String realServiceClass = realUserService.getClass().toString();
+        String proxyServiceClass = proxyService.getClass().toString();
+        if (realServiceClass.equals(proxyServiceClass)) {
+            System.out.println("\nReal service and proxy have same class:\n");
+        } else {
+            System.out.println("\nReal service and proxy have different class:\n");
+        }
+        System.out.println(realServiceClass);
+        System.out.println(proxyServiceClass);
 
         long timeStampBeforeCache = System.currentTimeMillis();
-        String nameBeforeCache = userService.findUserById(1L);
+        proxyService.findUserById(1L);
         System.out.println("\nExecution time before cache: " + (System.currentTimeMillis() - timeStampBeforeCache));
 
         long secondTimeStampBeforeCache = System.currentTimeMillis();
-        String nameAfterCache = userService.findUserById(1L);
+        proxyService.findUserById(1L);
         System.out.println("Execution time after cache: " + (System.currentTimeMillis() - secondTimeStampBeforeCache));
-
-        assert nameBeforeCache.equals(nameAfterCache);
     }
 }
